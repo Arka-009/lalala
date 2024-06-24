93d8349106cb56d778eeb8e29c455745f84c79f9
Arka-009 <bbcd7745@gmail.com>
2024-06-24 13:06:26 +0530

    add packages

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-const http=require('http')
-const server=http.createServer((req,res)=>
-    {if(req.url==='/'){
-        res.end('welcome to the home page')
-    }
-    else if(req.url==='/about'){
-        res.end('welcome to the about page')
-    }
-    else{
-    res.end('hello boy!!')}
-})
-server.listen(5000)
+const _ = require('lodash')
+const a1=[1,[2,[3]]]
+const p1=_.flattenDeep(a1)
+console.log(a1)
